bb618bee5311f6c9d842729091b8ba615ebb0b25
flange-ipb <34936695+flange-ipb@users.noreply.github.com>
2024-06-04 17:33:45 +0200

    update to InChI 1.07-RC2

--- pages/inchi.js
+++ pages/inchi.js
@@ -8,7 +8,7 @@
  */
 const inchiModulePromises = {
   "1.06": inchiModule106(),
-  "1.07-RC1": inchiModule107()
+  "1.07-RC2": inchiModule107()
 };
 
 const availableInchiVersions = Object.keys(inchiModulePromises);
