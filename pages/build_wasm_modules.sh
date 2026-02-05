30c9afebc92e68841972a9d0154bb974532bdba4
Felix Bänsch <168176402+fbaensch-beilstein@users.noreply.github.com>
2026-02-05 16:11:46 +0100

    Enable enhanced stereochmistry branch (WIP);

--- pages/build_wasm_modules.sh
+++ pages/build_wasm_modules.sh
@@ -18,6 +18,7 @@ source "${root_dir}/pages/utils.sh"
 build_inchi_wasm "1.06" "$inchi_versions_file" "$root_dir" "${root_dir}/inchi/util.c.patch"
 build_inchi_wasm "Latest" "$inchi_versions_file" "$root_dir"
 build_inchi_wasm "Latest with Molecular Inorganics" "$inchi_versions_file" "$root_dir"
+build_inchi_wasm "Latest with Enhanced Stereochemistry" "$inchi_versions_file" "$root_dir"
 
 # Build RInChI
 build_rinchi_wasm "$root_dir"
