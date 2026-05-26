39e6d81cb2c966bcc3ef88c71a94899618d73556
Felix Bänsch <168176402+fbaensch-beilstein@users.noreply.github.com>
2026-05-26 09:14:45 +0000

    refactor: rename Latest with Enhanced Stereochemistry to Dev with Enhanced Stereochemistry and track upstream dev branch

--- pages/build_wasm_modules.sh
+++ pages/build_wasm_modules.sh
@@ -19,7 +19,7 @@ build_inchi_wasm "1.06" "$inchi_versions_file" "$root_dir" "${root_dir}/inchi/ut
 build_inchi_wasm "1.07.5" "$inchi_versions_file" "$root_dir"
 build_inchi_wasm "Dev" "$inchi_versions_file" "$root_dir"
 build_inchi_wasm "Dev with Molecular Inorganics" "$inchi_versions_file" "$root_dir"
-build_inchi_wasm "Latest with Enhanced Stereochemistry" "$inchi_versions_file" "$root_dir"
+build_inchi_wasm "Dev with Enhanced Stereochemistry" "$inchi_versions_file" "$root_dir"
 
 # Build RInChI
 build_rinchi_wasm "$root_dir"
